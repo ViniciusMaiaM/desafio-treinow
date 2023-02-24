4c9be9aa82a34f10dc8d30478191a9d1b32c1555
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-02-24 13:21:41 -0300

    feat(workout): add delete route

--- src/routes/workout.routes.ts
+++ src/routes/workout.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { StoreWorkoutController } from "../app/controllers/studentControllers/storeWorkoutController";
+import { DeleteAccountController } from "../app/controllers/studentControllers/deleteAccountController";
 const authMiddleware = require('../app/middlewares/auth');
 
 const storeWorkoutController = new StoreWorkoutController();
+const deleteAccountController = new DeleteAccountController();
 const workoutRoutes = Router();
 workoutRoutes.use(authMiddleware);
 
 workoutRoutes.post("/store/workout", storeWorkoutController.handle);
+workoutRoutes.delete("/account/delete", deleteAccountController.handle);
 
 export {workoutRoutes};
